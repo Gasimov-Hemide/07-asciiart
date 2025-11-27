f31959039ce72c7c289e040ee58992adbd9dcdb0
Gasimov-Hemide <hemide.gasimov@edu.esiee.fr>
2025-11-27 10:33:25 +0000

    Exercice ASCIIART

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #### Imports et définition des variables globales
+"""
+Retourne la liste de tuples encodant une chaîne de caractères passée en argument
 
+Modules :
+    artcode_i(s)
+    artcode_r(s)
+"""
 # Mandatory for the recursive solution to work on large inputs
 import sys
 sys.setrecursionlimit(2000)
@@ -9,7 +15,9 @@ sys.setrecursionlimit(2000)
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """
+    retourne la liste de tuples encodant une chaîne de caractères passée en argument selon
+    un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +25,30 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
+
     # votre code ici
 
-    return [ ]
+    if not s:
+        return []
+
+    c = [s[0]]
+    o = [1]
+    n = len(s)
+
+    for k in range(1, n):
+        if s[k] == s[k-1]:
+            o[-1] += 1
+        else:
+            c.append(s[k])
+            o.append(1)
+
+    return list(zip(c, o))
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """
+    retourne la liste de tuples encodant une chaîne de caractères passée en argument 
+    selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,20 +56,35 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
+
     # votre code ici
 
     # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
+    if not s:
+        return []
 
-    return []
-    
+    # recherche nombre de caractères identiques au premier
+    premier_caractere = s[0]
+    longueur_segment = 0
+
+    for caractere in s:
+        if caractere == premier_caractere:
+            longueur_segment += 1
+        else:
+            break
+
+    premier_tuple = (premier_caractere, longueur_segment)
+
+    # appel récursif
+    return [premier_tuple] + artcode_r(s[longueur_segment:])
 
 #### Fonction principale
 
 
 def main():
+    """
+    Fait appel aux fonctions artcode_i(s) et artcode_r(s) pour vérifier leur bon fonctionnement
+    """
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
